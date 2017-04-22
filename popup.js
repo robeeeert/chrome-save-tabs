33596a82e33a1d6adeb137bf0962797ba859fd2c
Roberto Ciano <roberto@second-page.de>
2017-04-22 16:53:56 +0200

    encapsulate popup.js code

--- popup.js
+++ popup.js
@@ -1,59 +1,61 @@
-const savePlaintextButton = document.getElementById('save-plaintext')
-const saveJsonButton = document.getElementById('save-json')
-const allWindowsCheckbox = document.getElementById('save-all-windows')
+(function () {
+  const savePlaintextButton = document.getElementById('save-plaintext')
+  const saveJsonButton = document.getElementById('save-json')
+  const allWindowsCheckbox = document.getElementById('save-all-windows')
 
-/**
- * Since the proper way does not work (requestQuota always grants 0 bytes),
- * this copy-pasted workaround from http://stackoverflow.com/a/18197341/2452585
- * does the trick
- */
-function download(content, type, ext) {
-  const filename = `tablist.${ext}`
-  const element = document.createElement('a');
-  element.setAttribute('href', `data:${type};charset=utf-8,${encodeURIComponent(content)}`);
-  element.setAttribute('download', filename);
+  /**
+   * Since the proper way does not work (requestQuota always grants 0 bytes),
+   * this copy-pasted workaround from http://stackoverflow.com/a/18197341/2452585
+   * does the trick
+   */
+  function download(content, type, ext) {
+    const filename = `tablist.${ext}`
+    const element = document.createElement('a');
+    element.setAttribute('href', `data:${type};charset=utf-8,${encodeURIComponent(content)}`);
+    element.setAttribute('download', filename);
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
+    element.style.display = 'none';
+    document.body.appendChild(element);
 
-  element.click();
+    element.click();
 
-  document.body.removeChild(element);
-}
-
-/**
- * Makes sure the right tabs are selected based on the chosen settings
- */
-function handleSaveClick (cb) {
-  const query = {}
-  if (!allWindowsCheckbox.checked) {
-    query.windowId = chrome.windows.WINDOW_ID_CURRENT
+    document.body.removeChild(element);
   }
-  chrome.tabs.query(query, cb)
-}
 
-savePlaintextButton.onclick = () => {
-  const header = `Tab list created by Save Tabs on ${new Date()}\r\n`
-  handleSaveClick(tabs => {
-    const text = tabs.reduce((text, currentTab) => {
-      return `${text}\r\n${currentTab.url}`
-    }, header || '')
-    download(text, 'text/plain', 'txt')
-  })
-}
-
-saveJsonButton.onclick = () => {
-  const json = {
-    date: `${new Date()}`,
-    comment: 'Created by Save Tabs'
+  /**
+   * Makes sure the right tabs are selected based on the chosen settings
+   */
+  function handleSaveClick (cb) {
+    const query = {}
+    if (!allWindowsCheckbox.checked) {
+      query.windowId = chrome.windows.WINDOW_ID_CURRENT
+    }
+    chrome.tabs.query(query, cb)
   }
-  handleSaveClick(tabs => {
-    const jsonTabs = tabs.reduce((json, currentTab) => {
-      json[currentTab.title] = currentTab.url
-      return json
-    }, {})
 
-    json.tabs = jsonTabs
-    download(JSON.stringify(json), 'application/json', 'json')
-  })
-}
+  savePlaintextButton.onclick = () => {
+    const header = `Tab list created by Save Tabs on ${new Date()}\r\n`
+    handleSaveClick(tabs => {
+      const text = tabs.reduce((text, currentTab) => {
+        return `${text}\r\n${currentTab.url}`
+      }, header || '')
+      download(text, 'text/plain', 'txt')
+    })
+  }
+
+  saveJsonButton.onclick = () => {
+    const json = {
+      date: `${new Date()}`,
+      comment: 'Created by Save Tabs'
+    }
+    handleSaveClick(tabs => {
+      const jsonTabs = tabs.reduce((json, currentTab) => {
+        json[currentTab.title] = currentTab.url
+        return json
+      }, {})
+
+      json.tabs = jsonTabs
+      download(JSON.stringify(json), 'application/json', 'json')
+    })
+  }
+})()
